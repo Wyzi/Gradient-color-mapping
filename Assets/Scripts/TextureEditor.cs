2e9eba7aee395de86a6d15da874f2697442a7a4b
Wyzi <>
2020-02-05 10:30:41 +0300

    add gradient-color-mapping

---
+++ Assets/Scripts/TextureEditor.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+public class TextureEditor : EditorWindow
+{
+    Texture2D directoryTexR;
+    Texture2D directoryTexG;
+    Texture2D directoryTexB;
+
+    [MenuItem("Window/PNG Creator")]
+    static void OpenWindow()
+    {
+        EditorWindow.GetWindow(typeof(TextureEditor));
+    }
+    void OnGUI()
+    {
+        EditorGUIUtility.LookLikeInspector();
+
+        directoryTexR = (Texture2D)EditorGUILayout.ObjectField("Texture R Directory:", directoryTexR, typeof(Texture2D));
+        directoryTexG = (Texture2D)EditorGUILayout.ObjectField("Texture G Directory:", directoryTexG, typeof(Texture2D));
+        directoryTexB = (Texture2D)EditorGUILayout.ObjectField("Texture B Directory:", directoryTexB, typeof(Texture2D));
+
+        Color colorPixel = new Color();
+        Color[] color_gradient = new Color[256];
+        int gradient_x = 0;
+        float color_gray = 0;
+
+        if (GUILayout.Button("Create PNG"))
+        {
+            var tex = new Texture2D(directoryTexR.width, directoryTexR.height, TextureFormat.RGB24, false);
+            var tex_gradient = new Texture2D(256, 1, TextureFormat.RGB24, false);
+
+            for (int x = 0; x < directoryTexR.height; x++)
+            {
+                for (int y = 0; y < directoryTexR.height; y++)
+                {
+                    colorPixel.r = color_pixel(directoryTexR.GetPixel(x, y), ref color_gradient, ref gradient_x, ref color_gray);
+                    colorPixel.g = color_pixel(directoryTexG.GetPixel(x, y), ref color_gradient, ref gradient_x, ref color_gray);
+                    colorPixel.b = color_pixel(directoryTexB.GetPixel(x, y), ref color_gradient, ref gradient_x, ref color_gray);
+                    tex.SetPixel(x, y, colorPixel);
+                    }
+                }
+
+            for (int i = 0; i < 256; i++)
+            {
+                tex_gradient.SetPixel(i, 0, color_gradient[i]);
+            }
+
+            File.WriteAllBytes(EditorUtility.SaveFilePanel("Save texture...", Application.dataPath, "texture", "png"), tex.EncodeToPNG());
+            File.WriteAllBytes(EditorUtility.SaveFilePanel("Save texture...", Application.dataPath, "texture_gradient", "png"), tex_gradient.EncodeToPNG());
+        }
+    }
+     float color_pixel(Color texture_pixel,ref Color[] _color_gradient, ref int _gradient_x, ref float _color_gray)
+    {
+        float color_chanel=0;
+        if (_color_gradient.Contains(texture_pixel))
+        {
+            for (int i = 0; i < _color_gradient.Count(); i++)
+            {
+                if (_color_gradient[i] == texture_pixel)
+                {
+                    color_chanel= i * (1.0F / 255.0F);
+                    break;
+                }
+            }
+        }
+        else
+        {
+            _color_gradient[_gradient_x] = texture_pixel;
+            _gradient_x++;
+            color_chanel = _color_gray;
+            _color_gray = _color_gray + (1.0F / 255.0F);
+        }
+        return color_chanel;
+    }
+}
---
+++ Assets/Shaders/Color.shader
@@ -0,0 +1,57 @@
+﻿Shader "Custom/Color"
+{
+    Properties
+    {
+        _MainTex ("Albedo (RGB)", 2D) = "white" {}
+		_Color("Albedo (RGB)", 2D) = "white" {}
+		_R("R", Range(0, 1)) = 1
+		_G("G", Range(0, 1)) = 0
+		_B("B", Range(0, 1)) = 0
+		_GradientID("GradientID", Range(1, 256)) = 1
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 200
+
+        CGPROGRAM
+        // Physically based Standard lighting model, and enable shadows on all light types
+		  #pragma surface surf Standard fullforwardshadows
+
+        // Use shader model 3.0 target, to get nicer looking lighting
+        #pragma target 3.0
+
+        sampler2D _MainTex;
+		sampler2D _Color;
+		float _R;
+		float _G;
+		float _B;
+		float _GradientID;
+
+        struct Input
+        {
+			half2 uv_MainTex;
+        };
+
+
+        // Add instancing support for this shader. You need to check 'Enable Instancing' on materials that use the shader.
+        // See https://docs.unity3d.com/Manual/GPUInstancing.html for more information about instancing.
+        // #pragma instancing_options assumeuniformscaling
+        UNITY_INSTANCING_BUFFER_START(Props)
+            // put more per-instance properties here
+        UNITY_INSTANCING_BUFFER_END(Props)
+
+		void surf(Input IN, inout SurfaceOutputStandard o)
+        {
+			fixed r = tex2D (_MainTex, IN.uv_MainTex).r;
+			fixed g = tex2D(_MainTex, IN.uv_MainTex).g;
+			fixed b = tex2D(_MainTex, IN.uv_MainTex).b;
+			fixed color = r * _R + g * _G + b * _B;
+			float4 gragient= tex2D(_Color, float2(color,0));
+            o.Albedo = gragient.rgb;
+			o.Alpha = gragient.a;
+        }
+        ENDCG
+    }
+    FallBack "Diffuse"
+}
